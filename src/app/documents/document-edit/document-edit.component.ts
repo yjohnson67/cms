18b4609357afb2e0b113e8bb4818b992d2a983be
yjohnson67 <joh23004@byui.edu>
2026-06-13 23:12:34 -0500

    Completed week 8 Angular Forms assignment

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { Document } from '../document.model';
+import { DocumentsService } from '../documents.service';
 
 @Component({
   selector: 'cms-document-edit',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.css'
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit {
+  originalDocument: Document | null = null;
+  document: Document | null = null;
+  editMode: boolean = false;
+  id!: string;
 
+  constructor(
+    private documentService: DocumentsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+
+      if (!this.id) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalDocument = this.documentService.getDocument(this.id);
+
+      if (!this.originalDocument) {
+        return;
+      }
+
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+
+    const newDocument = new Document(
+      '',
+      value.name,
+      value.description,
+      value.url,
+      []
+    );
+
+    if (this.editMode && this.originalDocument) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+
+    this.router.navigate(['/documents']);
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
+  }
 }
